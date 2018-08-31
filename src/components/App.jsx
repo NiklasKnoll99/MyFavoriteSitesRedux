d2ed06361d41e5861aa895b572a5ea71fde6654b
Niklas Knoll <niklas.knoll@tobit.software>
2018-08-31 14:43:23 +0200

    ListItems are available

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import Intro from './intro/Intro';
 import PersonFinderContainer from '../containers/PersonFinderContainer';
 import UserListContainer from '../containers/UserListContainer';
 import NewFavForm from '../components/newFavForm/NewFavForm';
+import ListItems from '../components/listItem/ListItem'; // TEMP
+import ListItem from '../components/listItem/ListItem';
 
 const App = () => (
     <div>
